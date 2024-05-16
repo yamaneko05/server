8e7c6ed1568555a9869e62b7790217435966e1a3
yamaneko05 <o.yamaneko0331@gmail.com>
2024-05-16 21:57:45 +0900

    bio 追加とfaker日本語化

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('bio');
+            $table->string('icon_file')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
