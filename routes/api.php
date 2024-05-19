fc12e6867cf2236ab93290c217bd3a117be2e7f3
yamaneko05 <o.yamaneko0331@gmail.com>
2024-05-19 15:46:37 +0900

    web route fix

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,3 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::middleware(['web', 'auth:sanctum'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::apiResources([
-        '/users' => UserController::class,
-        '/posts' => PostController::class,
-        '/likes' => LikeController::class
-    ]);
-});
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+
+Route::prefix('/api')->middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResources([
+        '/users' => UserController::class,
+        '/posts' => PostController::class,
+        '/likes' => LikeController::class
+    ]);
 });
 
 require __DIR__.'/auth.php';
